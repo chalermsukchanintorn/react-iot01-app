b9899763237243fbedb7412f1099698e1c3f17de
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-04-01 12:18:56 +0700

    ทำหน้า Cal Num

--- src/views/CaculateNumber.jsx
+++ src/views/CaculateNumber.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { Box, Typography, Avatar, TextField } from '@mui/material'
+import { FormControl, InputLabel, Select, MenuItem, Button } from '@mui/material'
 
 function CaculateNumber() {
   return (
-    <div>CaculateNumber</div>
+    <>
+      <Box sx={{ height: '100vh', display: 'flex' }}>
+        <Box sx={{ width: '70%', boxShadow: 2, mx: 'auto', my: 'auto', p: 5 }}>
+          <Avatar src={'https://cdn.pixabay.com/photo/2023/03/13/15/21/iot-7850195_640.jpg'}
+            sx={{ width: 150, height: 150, mx: 'auto' }} />
+          <Typography variant='h5' sx={{ textAlign: 'center', mt: 2 }}>
+            เครื่องคิดเลข
+          </Typography>
+          <Typography sx={{ mt: 2 }}>
+            ตัวเลขตัวที่ 1
+          </Typography>
+          <TextField variant="outlined" fullWidth type="number" />
+          <Typography sx={{ mt: 2 }}>
+            ตัวเลขตัวที่ 2
+          </Typography>
+          <TextField variant="outlined" fullWidth type="number" />
+          <Typography sx={{ mt: 2 }}>
+            เลือกเครื่องหมายคำนวณ
+          </Typography>
+          <FormControl fullWidth>
+            <Select defaultValue={'+'}>
+              <MenuItem value={'+'}>+ บวก</MenuItem>
+              <MenuItem value={'-'}>- ลบ</MenuItem>
+              <MenuItem value={'x'}>x คูณ</MenuItem>
+              <MenuItem value={'÷'}>÷ หาร</MenuItem>
+            </Select>
+          </FormControl>
+          <Button fullWidth variant='contained' sx={{ mt: 3, backgroundColor: '#e3Ad72', height: 50 }}>
+            <Typography sx={{ textAlign: 'center' }}>
+              คำนวณ
+            </Typography>
+          </Button>
+          <Typography sx={{ textAlign: 'center', mt:4, color:'red', fontSize:100 }}>
+            0.00
+          </Typography>
+        </Box>
+      </Box>
+    </>
   )
 }
 
--- src/views/HomeMenu.jsx
+++ src/views/HomeMenu.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import { Box, Typography, Button, Avatar } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 function HomeMenu() {
   return (
     <>
-      <Box sx={{height: '100vh', display: 'flex'}}>
+      <Box sx={{ height: '100vh', display: 'flex' }}>
         <Box sx={{ width: '70%', boxShadow: 2, mx: 'auto', my: 'auto', p: 3 }}>
           <Avatar src={'https://cdn.pixabay.com/photo/2023/03/13/15/21/iot-7850195_640.jpg'}
             sx={{ width: 150, height: 150, mx: 'auto' }} />
-          <Typography variant='h3' sx={{ textAlign: 'center', mt: 2 }}>
+          <Typography variant='h5' sx={{ textAlign: 'center', mt: 2 }}>
             IoT Calculate by Ninnin SAU
           </Typography>
+
+          <Link to={'/calnum'}>
+            <Button fullWidth variant='contained' sx={{ mt: 2, backgroundColor: '#e30d72' }}>
+              <Typography variant='h6' sx={{ textAlign: 'center' }}>
+                Go to Calculate Number
+              </Typography>
+            </Button>
+          </Link>
+
+          <Button  component={Link} to={'/calmoneyshare'} fullWidth variant='contained' sx={{ mt: 2, backgroundColor: '#006533' }}>
+            <Typography variant='h6' sx={{ textAlign: 'center' }}>
+              Go to Calculate MoneyShare
+            </Typography>
+          </Button>
         </Box>
       </Box>
-
     </>
   )
 }
