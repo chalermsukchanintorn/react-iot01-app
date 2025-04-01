f04365ff284750e28bc754d70e0b71c99eacac56
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-04-01 10:58:42 +0700

    V.2: แก้หน้า HomeMenu

--- src/views/HomeMenu.jsx
+++ src/views/HomeMenu.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Box, Typography, Button, Avatar } from '@mui/material'
 
 function HomeMenu() {
   return (
-    <div>
-      Chanintorn Chalermsuk
-    </div>
+    <>
+      <Box sx={{height: '100vh', display: 'flex'}}>
+        <Box sx={{ width: '70%', boxShadow: 2, mx: 'auto', my: 'auto', p: 3 }}>
+          <Avatar src={'https://cdn.pixabay.com/photo/2023/03/13/15/21/iot-7850195_640.jpg'}
+            sx={{ width: 150, height: 150, mx: 'auto' }} />
+          <Typography variant='h3' sx={{ textAlign: 'center', mt: 2 }}>
+            IoT Calculate by Ninnin SAU
+          </Typography>
+        </Box>
+      </Box>
+
+    </>
   )
 }
 
